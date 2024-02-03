da9d8e3345d6d7589889a8a8e5a2097fac691dbc
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-02-03 21:00:53 +0100

    1. Added DbContext and DesignTimeDbContextFactory

---
+++ Infrastructure/Data/Infrastrcture.Data/ContractContext.cs
@@ -0,0 +1,15 @@
+﻿using ContractBC.ContractAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastrcture.Data
+{
+	public class ContractContext : DbContext
+	{
+        public ContractContext(DbContextOptions options): base(options)
+        {
+           
+        }
+
+        public DbSet<Contract> Contracts => Set<Contract>();
+    }
+}
---
+++ Infrastructure/Data/Infrastrcture.Data/ContractContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastrcture.Data
+{
+	public class ContractContextFactory : IDesignTimeDbContextFactory<ContractContext>
+	{
+		public ContractContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder();
+			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PubContractDataMigrationsTest;Trusted_Connection=True;MultipleActiveResultSets=true;");
+
+			return new ContractContext(optionsBuilder.Options);
+		}
+	}
+}
